28d748acb20218075f3a0809ca98f1b9a34f1ba0
Aalaa <aalaabadr268@gmail.com>
2023-10-23 12:35:37 +0300

    product views and model functions

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
+
 # from sqlalchemy import func
 
 
@@ -13,5 +15,46 @@ class Product(db.Model):
     price = db.Column(db.Float)
     image = db.Column(db.String, nullable=True)
     instock = db.Column(db.Boolean)
-    created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    modified = db.Column(db.DateTime(timezone=True), server_onupdate=db.func.now(), server_default=db.func.now())
+    created = db.Column(db.DateTime, server_default=db.func.now())
+    modified = db.Column(db.DateTime, server_onupdate=db.func.now(), server_default=db.func.now())
+
+    @property
+    def img_url(self):
+        return url_for('static', filename=f'images/{self.image}')
+
+    @classmethod
+    def get_products(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_product(cls, id):
+        return cls.query.get_or_404(id)
+
+    @classmethod
+    def save_product(cls, request_data):
+        product = cls(name=request_data.form['name'],
+                      image=request_data.form['file'],
+                      price=request_data.form['price'],
+                      desc=request_data.form['desc'],
+                      instock=bool(request_data.form['instock']))
+
+        db.session.add(product)
+        db.session.commit()
+        return product
+
+    @classmethod
+    def update_product(cls, request_data, id):
+        product = cls.query.get_or_404(id)
+        product.name = request_data.form['name']
+        product.price = request_data.form['price']
+        product.image = request_data.form['file']
+        product.desc = request_data.form['desc']
+        product.instock = bool(request_data.form['instock'])
+
+        db.session.commit()
+        return product
+
+    def delete_product(self):
+        db.session.delete(self)
+        db.session.commit()
+        return True
--- app/products/views.py
+++ app/products/views.py
@@ -1,9 +1,47 @@
-from flask import render_template
+from flask import request, redirect, url_for, render_template
 
+from app.models import Product
 from app.products import product_blueprint
 
 
-# use blueprint to collect related urls
-@product_blueprint.route('/', endpoint='home')
+@product_blueprint.route('/', endpoint='home', methods=['GET'])
 def home():
     return render_template('layouts/index.html')
+
+
+@product_blueprint.route('/products', endpoint='products', methods=['GET'])
+def products():
+    products = Product.get_products()
+    return render_template('product/products.html', products=products)
+
+
+@product_blueprint.route('/product/<int:id>', endpoint='product', methods=['GET'])
+def product(id):
+    product = Product.get_product(id)
+    return render_template('product/product.html', product=product)
+
+
+@product_blueprint.route('/product/add', methods=['GET', 'POST'], endpoint='add')
+def create():
+    if request.method == 'POST':
+        Product.save_product(request_data=request)
+        return redirect(url_for('products.products'))
+
+    return render_template('product/add.html')
+
+
+@product_blueprint.route('/product/update/<int:id>', methods=['GET', 'POST'], endpoint='update')
+def update(id):
+    product = Product.get_product(id)
+    if request.method == 'POST':
+        Product.update_product(id=id, request_data=request)
+        return redirect(url_for('products.product', id=product.id))
+
+    return render_template('product/update.html', product=product)
+
+
+@product_blueprint.route('/product/delete/<int:id>', endpoint='delete')
+def delete(id):
+    product = Product.get_product(id)
+    product.delete_product()
+    return redirect(url_for('products.products'))
